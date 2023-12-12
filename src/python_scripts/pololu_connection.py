cb41a2fcee07189552a02de6e6ea5f3baf01dfca
Alejandro Marquez <alejandromarquez@berkeley.edu>
2023-12-12 11:08:03 -0800

    Changes to python scripts for bluetooth

--- src/python_scripts/pololu_connection.py
+++ src/python_scripts/pololu_connection.py
@@ -4,12 +4,24 @@ import sys
 from bleak import BleakScanner
 from bleak import BleakClient
 
-filename_suffix = "_commands.txt"
+transmit_filename_suffix = "_commands.txt"
+receive_filename_suffix = "_replies.txt"
 
 
 def write_message(robot_name, message):
-    with open(robot_name + filename_suffix, "w") as file:
+    with open(robot_name + transmit_filename_suffix, "w") as file:
         file.write(message)
+
+def read_message(robot_name):
+    try:
+        with open(robot_name + receive_filename_suffix, "r") as file:
+            message = file.read()
+    except FileNotFoundError:
+        message = ""
+
+    clear_file(robot_name + receive_filename_suffix)
+    return message
+    
 
 def set_name(robot_name, name):
     write_message(robot_name, "SET_NAME:"+ name)
@@ -24,6 +36,19 @@ async def send_message(client, serial_characteristic, message):
     await client.write_gatt_char(serial_characteristic, carriage_return_byte)
     print(f"Sent message: {message}")
 
+received_data = ""
+async def receive_message(sender, data):
+    global received_data
+    global receive_filename
+    clear_file(receive_filename)
+    received_data += data.decode('utf-8')
+    if received_data.endswith('\r'):
+        with open(receive_filename, "w") as file:
+            file.write(received_data)
+
+        print(f"Received complete message: {received_data}")
+        received_data = ""
+
 def clear_file(filename):
     open(filename, 'w').close()
 
@@ -32,22 +57,24 @@ def ensure_file_exists(filename):
     if not os.path.exists(filename):
         open(filename, 'w').close()
 
-async def monitor_file_for_messages(message_file, client):
+async def monitor_files_for_messages(commands_file, client):
     serial_characteristic = "None"
     for service in client.services:
         print(f"Service: {service.uuid}")
         for characteristic in service.characteristics:
             serial_characteristic = characteristic.uuid
             print(f"Serial Characteristic: {serial_characteristic}")
+    
+    await client.start_notify(serial_characteristic, receive_message)
     print("Monitoring messages...")
     while True:
         await asyncio.sleep(0.05)  # Sleep for 1 second
         try:
-            with open(message_file, 'r') as file:
+            with open(commands_file, 'r') as file:
                 content = file.read()
                 if content:
                     await send_message(client, serial_characteristic, content) # New message received, send it to the device
-                    clear_file(message_file) # Clear the file after sending the message
+                    clear_file(commands_file) # Clear the file after sending the message
         except FileNotFoundError:
             continue  # File not found, keep checking
 
@@ -58,24 +85,29 @@ async def find_device(device_name):
             return device
     return None
 
-async def connect_and_maintain(address, filename):
+async def connect_and_maintain(address, commands_file):
     async with BleakClient(address) as client:
         if client.is_connected:
             print(f"Connected to {address}")
             await client.get_services() # Perform service discovery
-            await monitor_file_for_messages(filename, client)
+            await monitor_files_for_messages(commands_file, client)
         else:
             print("Failed to connect.")
 
 if __name__ == "__main__":
+    global receive_filename
     device_name = sys.argv[1]
-    filename = device_name.lower() + filename_suffix
-    ensure_file_exists(filename)
+    transmit_filename = device_name.lower() + transmit_filename_suffix
+    ensure_file_exists(transmit_filename)
+
+    receive_filename = device_name.lower() + receive_filename_suffix
+    ensure_file_exists(receive_filename)
+
     loop = asyncio.get_event_loop()
     target_device = loop.run_until_complete(find_device(device_name))
 
     if target_device:
         print(f"Found target device: {target_device.name}, Address: {target_device.address}")
-        loop.run_until_complete(connect_and_maintain(target_device.address, filename))
+        loop.run_until_complete(connect_and_maintain(target_device.address, transmit_filename))
     else:
         print("Target device not found.")
--- src/python_scripts/pololu_master.py
+++ src/python_scripts/pololu_master.py
@@ -1,16 +1,21 @@
 import pololu_connection as pololu
 import time
 
-pololu_names = ["alex", "ashley"]
+pololu_names = ["alex"]
 pololu_prefix = "pololu_"
 
-for name in pololu_names:
-    pololu.set_name(pololu_prefix + name, name)
-counter = 0
+# for name in pololu_names:
+#     pololu.set_name(pololu_prefix + name, name)
 while True:
     for name in pololu_names:
         robot_name = pololu_prefix + name
-        message = f"{counter}"
-        pololu.write_message(robot_name, message)
-    counter += 1
-    time.sleep(1)
+
+        transmitted_message = input(f"Message to {name.title()}: ")
+        if (transmitted_message != ""):
+            pololu.write_message(robot_name, transmitted_message)
+
+        received_mssage = pololu.read_message(robot_name)
+        while not received_mssage:
+            received_mssage = pololu.read_message(robot_name)
+        print(f"{name.title()} replied: {received_mssage}")
+    
